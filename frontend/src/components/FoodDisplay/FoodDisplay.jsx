1d60a7d324a6d1e2ffd4c31b83eac87bec8572a1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-19 09:28:45 +0530

    Food filter by menu done

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,8 +11,10 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
+              if(category==='All' || category===item.category){
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-            })}
+              }
+                            })}
         </div>
     </div>
   )

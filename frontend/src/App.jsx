752d41f57be4c4e67aca9beb2b809deb642ed923
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-19 16:15:35 +0530

    Footer done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,11 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -15,6 +17,8 @@ const App = () => {
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 

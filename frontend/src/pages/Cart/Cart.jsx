6dc33579d855dfb7191368bde92c459c025c937c
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-22 13:51:38 +0530

    Cart bottom section HTML structure done

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -20,16 +20,52 @@ const Cart = () => {
         <hr />
         {food_list.map((item,index)=>{
           if(cartItems[item._id]>0){
-            return <div className="cart-items-title cart-items-item">
+            return <div>
+              <div className="cart-items-title cart-items-item">
               <img src={item.image} alt="" />
               <p>{item.name}</p>
-              <p>{item.price}</p>
+              <p>${item.price}</p>
               <p>{cartItems[item._id]}</p>
-              <p>{item.price*cartItems[item._id]}</p>
-              <p>x</p>
+              <p>${item.price*cartItems[item._id]}</p>
+              <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
             </div>
+            <hr />
+            </div>
+
+            
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-detail">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-detail">
+              <b>Total</b>
+              <b>{0}</b>
+            </div> 
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, enter it here</p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='Promo Code'/>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )

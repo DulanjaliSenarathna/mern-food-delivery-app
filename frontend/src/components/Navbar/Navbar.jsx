793b67717323f0dbeaf122238d259594b90cb535
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-22 08:57:05 +0530

    Smooth scroll added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from './../../assets/assets';
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -10,10 +11,10 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo' />
         <ul className="navbar-menu">
-            <li onClick={()=> setMenu('home')} className={menu === 'home'?'active':''}>home</li>
-            <li onClick={()=> setMenu('menu')} className={menu === 'menu'?'active':''}>menu</li>
-            <li onClick={()=> setMenu('mobile-app')} className={menu === 'mobile-app'?'active':''}>mobile-app</li>
-            <li onClick={()=> setMenu('contact-us')} className={menu === 'contact-us'?'active':''}>contact us</li>
+            <Link to='/' onClick={()=> setMenu('home')} className={menu === 'home'?'active':''}>home</Link>
+            <a href='#explore-menu' onClick={()=> setMenu('menu')} className={menu === 'menu'?'active':''}>menu</a>
+            <a href='#app-download' onClick={()=> setMenu('mobile-app')} className={menu === 'mobile-app'?'active':''}>mobile-app</a>
+            <a href='#footer' onClick={()=> setMenu('contact-us')} className={menu === 'contact-us'?'active':''}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />

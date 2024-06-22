929a96accff24fc59201c696d776c751d435d889
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-22 15:21:38 +0530

    Updated cart icon logic

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from './../../assets/assets';
 import {Link} from 'react-router-dom'
+import { StoreContext } from './../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
 
   const [menu, setMenu] = useState('home');
+
+  const {getTotalCartAmount} = useContext(StoreContext);
 
   return (
     <div className='navbar'>
@@ -20,7 +23,7 @@ const Navbar = ({setShowLogin}) => {
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                <div className="dot"></div>
+                <div className={getTotalCartAmount()===0?'':'dot'}></div>
             </div>
             <button onClick={()=> setShowLogin(true)}>sign in</button>
         </div>

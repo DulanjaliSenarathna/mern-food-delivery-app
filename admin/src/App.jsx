4424395f59c01b38937fee89aec2296dcc3ba898
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-25 21:31:38 +0530

    Navbar done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
+      </div>
     </div>
   )
 }

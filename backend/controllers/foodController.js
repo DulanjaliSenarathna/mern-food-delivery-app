6f4b48c35604c01ef2d4ef2964e5c358be492313
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-25 18:23:48 +0530

    Delete food item API endpoint done

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -36,4 +36,19 @@ const listFood = async (req,res) =>{
     }
 }
 
-export {addFood, listFood}
+// remove food item
+
+const removeFood = async (req,res)=>{
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,()=>{})
+
+        await foodModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:'Food Removed'})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:'Error'})
+    }
+}
+
+export {addFood, listFood, removeFood}
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood, listFood } from '../controllers/foodController.js'
+import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer'
 
 const foodRouter = express.Router();
@@ -16,5 +16,6 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage})
 foodRouter.post('/add',upload.single('image'),addFood)
 foodRouter.get('/list',listFood)
+foodRouter.post('/remove', removeFood)
 
 export default foodRouter;

d13282e3e7913eab6a3150390fa433eefdd83575
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-30 12:04:38 +0530

    Side bar done with responsive

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
   )
 }
 

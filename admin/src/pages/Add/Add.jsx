d7297958812c41dfd252af0cc256b8ba1a5a6648
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-06-30 13:42:29 +0530

    Add page design done

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './Add.css'
+import { assets } from '../../assets/assets'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='add'>
+        <form  className="flex-col">
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor="image">
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input type="file" id='image' hidden required />
+            </div>
+            <div className="add-product-name flex-col">
+                <p>Product name</p>
+                <input type="text" name='name' placeholder='Type Here' />
+            </div>
+            <div className="add-product-description flex-col">
+                <p>Product description</p>
+                <textarea name="description" rows='6' placeholder='Write content here' required></textarea>
+            </div>
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product category</p>
+                    <select name="category">
+                        <option value="Salad">Salad</option>
+                        <option value="Rolls">Rolls</option>
+                        <option value="Deserts">Deserts</option>
+                        <option value="Sandwich">Sandwich</option>
+                        <option value="Cake">Cake</option>
+                        <option value="Pure Veg">Pure Veg</option>
+                        <option value="Pasta">Pasta</option>
+                        <option value="Noodles">Noodles</option>
+                    </select>
+                </div>
+                <div className="add-price flex-col">
+                    <p>Product price</p>
+                    <input type="number" name='price' placeholder='$20'/>
+                </div>
+            </div>
+            <button type='submit' className='add-btn'>ADD</button>
+        </form>
+    </div>
   )
 }
 

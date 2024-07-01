44980a4fddfdbb1ec8aeb1ab0a7fca68cd98ad06
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-01 08:59:34 +0530

    Fetch food list done

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const List = () => {
+
+  const url = 'http://localhost:4000';
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () =>{
+    const response = await axios.get(`${url}/api/food/list`)
+    console.log(response.data);
+    if(response.data.success){
+      setList(response.data.data)
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(()=>{
+    fetchList();
+  },[])
   return (
-    <div>List</div>
+    <div>
+
+    </div>
   )
 }
 

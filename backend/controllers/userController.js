4ee44738c12bf95a27e49cef75cfb551e8791561
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-01 20:23:04 +0530

    User login done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,27 @@ import bcrypt from 'bcrypt'
 import validator from 'validator'
 
 //login user
-const loginUser = (req,res) =>{
+const loginUser = async (req,res) =>{
+    const {email, password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
 
+        if(!user){
+           return res.json({success:false, message:'User does not exist'}) 
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.json({success:false, message:'Invalid credentials'})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true, token})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:'Error'})
+    }
 }
 
 const createToken = (id) =>{

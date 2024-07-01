bededecb6e4da9744ed684d9ed30e40954cff5c1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-01 14:25:15 +0530

    Added all food items

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -11,12 +11,22 @@ const List = () => {
 
   const fetchList = async () =>{
     const response = await axios.get(`${url}/api/food/list`)
-    console.log(response.data);
+   
     if(response.data.success){
       setList(response.data.data)
     }
     else{
       toast.error("Error")
+    }
+  }
+
+  const removeFood = async (foodId) =>{
+    const response = await axios.post(`${url}/api/food/remove`,{id:foodId})
+    await fetchList();
+    if(response.data.success){
+      toast.success(response.data.message)
+    }else{
+      toast.error('Error');
     }
   }
 
@@ -41,7 +51,7 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>${item.price}</p>
-              <p className='cursor'>X</p>
+              <p onClick={()=> removeFood(item._id)} className='cursor'>X</p>
             </div>
           )
         })}

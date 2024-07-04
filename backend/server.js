6ff5f85e6ac0bd909261a49a934ed3c7acacb466
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-04 12:12:49 +0530

    Add to cart done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config';
+import cartRouter from './routes/cartRoute.js';
 
 //app config
 const app = express()
@@ -20,6 +21,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use('/api/user', userRouter)
+app.use('/api/cart', cartRouter)
 
 app.get("/",(req,res)=>{
         res.send("API working")

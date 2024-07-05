72df8d762a8c16e8332d51910840e3e24b714502
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-05 16:38:08 +0530

    User navigation done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = ({setShowLogin}) => {
             :<div className='navbar-profile'>
               <img src={assets.profile_icon} alt="" />
               <ul className="nav-profile-dropdown">
-                <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                <li onClick={()=> navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
                 <hr />  
                 <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
               </ul>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../components/context/StoreContext';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const PlaceOrder = () => {
   const {getTotalCartAmount, token, food_list, cartItems, url} = useContext(StoreContext);
@@ -49,6 +50,16 @@ const PlaceOrder = () => {
       alert('Error')
     }
   }
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart')
+    }else if(getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>

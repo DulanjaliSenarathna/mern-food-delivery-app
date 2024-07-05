d1b7d50033b3dcb1ae89fa60adc45b4906c5f13f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-05 16:52:01 +0530

    get list of orders for backend api created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -82,4 +82,15 @@ const userOrders = async (req,res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders}
+// listing orders for admin panel
+const listOrders = async (req,res) =>{
+   try {
+    const orders = await orderModel.find({});
+    res.json({success:true, data:orders})
+   } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})  
+   } 
+}
+
+export {placeOrder, verifyOrder, userOrders,listOrders}
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,12 +1,13 @@
 import express from "express"
 import authMiddleware from './../middleware/auth.js';
-import { placeOrder, verifyOrder, userOrders } from "../controllers/orderController.js";
+import { placeOrder, verifyOrder, userOrders,listOrders } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userorders",authMiddleware,userOrders)
+orderRouter.get('/list',listOrders)
 
 export default orderRouter;
 

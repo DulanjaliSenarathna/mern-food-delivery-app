c34fb4c9b2d80fde11cfbb10906cd929b61b92b6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-05 22:46:44 +0530

    Change base url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://mern-food-delivery-app-one.vercel.app/';
 
   return (
     <div>

fa8ca141ae983e5c34d8d9fda72f5397692ae4c5
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-07-05 14:26:17 +0530

    Payment verification done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 // Placing user order for frontend
 const placeOrder = async (req, res) =>{
 
-    const frontend_url = 'http://localhost:5173/';
+    const frontend_url = 'http://localhost:5173';
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -55,4 +55,20 @@ const placeOrder = async (req, res) =>{
     }
 }
 
-export {placeOrder}
+const verifyOrder = async (req, res) =>{
+    const {orderId, success} = req.body;
+    try {
+        if(success=='true'){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true, message:"Paid"})
+        }else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false, message:"Not Paid"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+export {placeOrder, verifyOrder}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
+import Verify from './pages/Verify/Verify'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/order' element={<PlaceOrder/>}/>
+        <Route path='/verify' element={<Verify/>}/>
       </Routes>
     </div>
     <Footer/>
